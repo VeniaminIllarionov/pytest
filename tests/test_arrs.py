0eec8c76f8502de00b184ef4b7db275c942c1478
veniamin <venya957@gmail.com>
2024-01-26 10:49:35 +0300

    fix 100

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,4 +1,4 @@
-from pytest_proj.utils import arrs
+from utils import arrs
 
 
 def test_get():
@@ -9,3 +9,8 @@ def test_get():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([], 0) == []
+    assert arrs.my_slice([]) == []
+    assert arrs.my_slice([3, 4, 5, 6, 7], 10) == []
+    assert arrs.my_slice([1, 2, 5, 7, 9], None) == [1, 2, 5, 7, 9]
+    assert arrs.my_slice([1, 2, 3, 5, 6, 7, 0], -5) == [3, 5, 6, 7, 0]
